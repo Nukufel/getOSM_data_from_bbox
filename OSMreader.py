47733d0b5a812438aab46a9c1bbfc396bafacc50
Nukufel <niklas.vogel@bluewin.ch>
2023-10-17 12:54:58 +0200

    reformat code

--- OSMreader.py
+++ OSMreader.py
@@ -6,21 +6,18 @@ boundary_geojson = gpd.read_file('boundary.geojson')
 
 point = 47.22347, 8.81724
 dist = 1500
-radius = ox.features_from_point(point, {'building': True, 'room': True, 'door': True, 'indoor': True, 'amenity': True, 'sport': True, 'tourism': True, 'fauntain': True, 'leisure': True}, dist=dist)
 
+radius = ox.features_from_point(point, {'building': True, 'room': True, 'door': True, 'indoor': True, 'amenity': True,
+                                        'sport': True, 'tourism': True, 'fauntain': True, 'leisure': True}, dist=dist)
 radius_map = radius.map(lambda x: str(x) if isinstance(x, list) else x)
 
 map_final = gpd.clip(radius_map, boundary_geojson)
-
 map_final.to_file("output.geojson", driver="GeoJSON")
-
 
 geojson_file_path = 'output.geojson'
 
 with open(geojson_file_path, 'r') as geojson_file:
     myGeoJSON = json.load(geojson_file)
-
-
 
 for feature in myGeoJSON["features"]:
     obj = feature["properties"]
@@ -39,10 +36,5 @@ for feature in myGeoJSON["features"]:
     obj["@id"] = typevalue + "/" + str(idvalue)
     feature['id'] = typevalue + "/" + str(idvalue)
 
-output_geojson_file_path = 'output.geojson'
-
-with open(output_geojson_file_path, 'w') as output_geojson_file:
+with open(geojson_file_path, 'w') as output_geojson_file:
     json.dump(myGeoJSON, output_geojson_file, indent=2, ensure_ascii=False)
-
-
-#47.22347/8.81724 mid
