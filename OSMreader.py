d3a71eb04147af36e842652152bbbd5dde18a227
Nukufel <niklas.vogel@bluewin.ch>
2024-03-18 16:06:06 +0100

    some changes

--- OSMreader.py
+++ OSMreader.py
@@ -2,48 +2,48 @@ from shapely.geometry import shape
 import osmnx as ox
 import json
 
+OUTPUT_FILE_NAME = "output.geojson"
+BOUNDARY_FILE_NAME = "boundary.geojson"
+RELEVANT_TAGS = {'building': True, 'room': True, 'door': True, 'indoor': True, 'amenity': True, 'sport': True, 'tourism': True, 'fauntain': True, 'leisure': True}
 # turning of cache
-ox.settings.use_cache=False
+ox.settings.use_cache = False
 
 # makeig a polygon from the boundary file
-with open("boundary.geojson") as f:
-  features = json.load(f)["features"]
+with open(BOUNDARY_FILE_NAME) as f:
+    features = json.load(f)["features"]
 
 boundaries = shape(features[0]["geometry"])
 
 # getting the data from osm
-data_semi_final = ox.features.features_from_polygon(boundaries,
-                                        {'building': True, 'room': True, 'door': True, 'indoor': True, 'amenity': True,
-                                        'sport': True, 'tourism': True, 'fauntain': True, 'leisure': True})
-
-map_final = data_semi_final.map(lambda x: str(x) if isinstance(x, list) else x)
+data_for_map = ox.features.features_from_polygon(boundaries, RELEVANT_TAGS)
+formatted_map_data = data_for_map.map(lambda x: str(x) if isinstance(x, list) else x)
 
 # writing data to geojson
-map_final.to_file("output.geojson", driver="GeoJSON")
+formatted_map_data.to_file(OUTPUT_FILE_NAME, driver="GeoJSON", na="drop")
 
 # getting geojson to edit
-geojson_file_path = 'output.geojson'
-with open(geojson_file_path, 'r') as geojson_file:
-    myGeoJSON = json.load(geojson_file)
+with open(OUTPUT_FILE_NAME, 'r') as geojson_file:
+    my_geojson = json.load(geojson_file)
 
 # editing goejson date to delete None objects and changing osmid to @id and id
-for feature in myGeoJSON["features"]:
-    obj = feature["properties"]
+for feature in my_geojson["features"]:
+    feat_props = feature["properties"]
     keys_to_delete = []
-    idvalue = ""
-    typevalue = ""
-    for key, attrValue in obj.items():
+
+    for key, attrValue in feat_props.items():
         if not attrValue:
             keys_to_delete.append(key)
-        if key == "element_type":
-            typevalue = attrValue
-        if key == "osmid":
-            idvalue = attrValue
+
     for key in keys_to_delete:
-        del obj[key]
-    obj["@id"] = typevalue + "/" + str(idvalue)
-    feature['id'] = typevalue + "/" + str(idvalue)
+        del feat_props[key]
+
+    element_type = str(feat_props["element_type"])
+    osm_id = str(feat_props["osmid"])
+    id = f"{element_type}/{osm_id}"
+
+    feat_props["@id"] = id
+    feature['id'] = id
 
 # saving edited geojson
-with open(geojson_file_path, 'w') as output_geojson_file:
-    json.dump(myGeoJSON, output_geojson_file, indent=2, ensure_ascii=False)
+with open(OUTPUT_FILE_NAME, 'w') as output_geojson_file:
+    json.dump(my_geojson, output_geojson_file, indent=2, ensure_ascii=False)
